4c6ee351a9ceeb10166e7f1623b6586ee3a17c62
charsle3 <jpkingthe3rd@gmail.com>
2026-03-07 12:32:46 -0800

    w01 code assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,19 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // create an empty array of doubles the size of length.
+        // create a for loop, starting at 1 that increments by one and runs while less than or equal to length.
+        // inside the loop, set the value of the doubles array indexed at the iteration count minus 1 equal to number multiplied 
+        // by the current iteration count.
 
-        return []; // replace this return statement with your own
+        var multiples = new double[length];
+
+        for (int i = 1; i <= length; i++)
+        {
+            multiples[i - 1] = number * i;
+        }
+
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,9 +32,12 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // create a tempList that is the range of values specified by amount, copied from the end of data.
+        // remove that same range from data
+        // insert the tempList into the beginning of data
+
+        List<int> tempList = data.GetRange(data.Count - amount, amount);
+        data.RemoveRange(data.Count - amount, amount);
+        data.InsertRange(0, tempList);
     }
 }
